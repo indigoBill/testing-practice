2119a145d1f48063cb21bf251c18ed3c9083a3bd
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-04-29 20:03:51 -0400

    Refactor caesarCipher function

--- testing_practice.js
+++ testing_practice.js
@@ -7,29 +7,57 @@ export function reverseString(string){
 }
 
 export const calculator = {
+    valueCheck : function(num1, num2){
+        if(typeof(num1) === 'number' && typeof(num2) === 'number') return [num1, num2];
+        else return;
+    },
 
     add : function(num1, num2){
-        if(typeof(num1) === 'number' && typeof(num2) === 'number') return num1 + num2;
-        else return 'Invalid Input';
+        if(this.valueCheck(num1, num2)) return num1 + num2;
     },
 
     subtract : function(num1, num2){
-        if(typeof(num1) === 'number' && typeof(num2) === 'number') return num1 - num2;
-        else return 'Invalid Input';
+        if(this.valueCheck(num1, num2)) return num1 - num2;
     },
 
     divide : function(num1, num2){
-        if(typeof(num1) === 'number' && typeof(num2) === 'number'){
-            if(num2 === 0) return;
-            else return num1 / num2;
-        }else{
-            return 'Invalid Input';
-        }
+        if(num2 !== 0 && this.valueCheck(num1, num2)) return num1 / num2;
     },
 
     multiply : function(num1, num2){
-        if(typeof(num1) === 'number' && typeof(num2) === 'number') return num1 * num2;
-        else return 'Invalid Input';
+        if(this.valueCheck(num1, num2)) return num1 * num2;
     },
 }
 
+export function caesarCipher(string, shiftFactor = 0){
+    const lettersOnly = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const lettersAndPunctuation = ['.',',','?','!','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let punctuationIncluded = false;
+
+    let index = 0;
+    const newString = [];
+
+    if(string.match(/[.,?!]/)) punctuationIncluded = true;
+
+    while(newString.length < string.length){
+        const char = string[index];
+
+        if(char === ' '){
+            newString.push(char);
+        }else{
+            const plainAlphabet = punctuationIncluded ? lettersAndPunctuation.slice() : lettersOnly.slice();
+            let isUpperCase = (char === char.toUpperCase() && !(char.match(/[.,?!]/))) ? true : false;
+            let arrayPosition = punctuationIncluded ? lettersAndPunctuation.indexOf(char.toLowerCase()) : lettersOnly.indexOf(char.toLowerCase());
+
+            arrayPosition += shiftFactor;
+            arrayPosition = (arrayPosition >= plainAlphabet.length) ? arrayPosition - plainAlphabet.length : arrayPosition;
+
+            if(isUpperCase) newString.push(plainAlphabet[arrayPosition].toUpperCase());
+            else newString.push(plainAlphabet[arrayPosition]);
+        }
+
+        index++;
+    }
+    
+    return newString.join('');
+}
